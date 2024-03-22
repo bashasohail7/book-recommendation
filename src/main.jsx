2335fe774acf4b8defd262923b9996cc869f4da6
5682 <sohail.shaik@wisseninfotech.com>
2024-03-22 20:13:38 +0530

    removed unnecessary dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App'; // Your root component
 import './index.css'
 import { BookProvider } from './context/BookContext';
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <QueryClientProvider client={queryClient}>
-      <BookProvider>
-
-        <App />
-      </BookProvider>
-    </QueryClientProvider>
+    <BookProvider>
+      <App />
+    </BookProvider>
 
   </React.StrictMode>,
 )
